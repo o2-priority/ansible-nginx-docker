049906645c8b1380a1f3e40be25f995c6f63748e
Ogonna Iwunze <ogonna.iwunze@gmail.com>
2016-04-22 16:32:14 +0100

    [OI] Add task to remove service object nginx config. Add tests for new tasks

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,7 +1,8 @@
 require 'spec_helper'
 
 nginx_docker_conf_dir = '/data/nginx'
-nginx_docker_service_name = 'jenkins-8080'
+nginx_docker_service_name_add = 'jenkins-8080'
+nginx_docker_service_name_remove = 'jenkins-50000'
 
 %W(
   #{nginx_docker_conf_dir}
@@ -20,18 +21,33 @@ end
 %W(
   #{nginx_docker_conf_dir}/conf.d/services.conf
   #{nginx_docker_conf_dir}/servers/common.conf
-  #{nginx_docker_conf_dir}/templates/#{nginx_docker_service_name}-include.conf.ctmpl
-  #{nginx_docker_conf_dir}/templates/#{nginx_docker_service_name}-upstream.conf.ctmpl
-  #{nginx_docker_conf_dir}/includes/common/#{nginx_docker_service_name}.conf
-  #{nginx_docker_conf_dir}/upstreams/#{nginx_docker_service_name}.conf
+  #{nginx_docker_conf_dir}/templates/#{nginx_docker_service_name_add}-include.conf.ctmpl
+  #{nginx_docker_conf_dir}/templates/#{nginx_docker_service_name_add}-upstream.conf.ctmpl
+  #{nginx_docker_conf_dir}/includes/common/#{nginx_docker_service_name_add}.conf
+  #{nginx_docker_conf_dir}/upstreams/#{nginx_docker_service_name_add}.conf
 ).each do |f|
   describe file(f) do
     it { should be_file }
   end
 end
 
+%W(
+  #{nginx_docker_conf_dir}/templates/#{nginx_docker_service_name_remove}-include.conf.ctmpl
+  #{nginx_docker_conf_dir}/templates/#{nginx_docker_service_name_remove}-upstream.conf.ctmpl
+  #{nginx_docker_conf_dir}/includes/common/#{nginx_docker_service_name_remove}.conf
+  #{nginx_docker_conf_dir}/upstreams/#{nginx_docker_service_name_remove}.conf
+).each do |f|
+  describe file(f) do
+    it { should_not exist }
+  end
+end
+
 describe docker_container('nginx') do
   it { should be_running }
+end
+
+describe command("curl -s http://localhost:8500/v1/catalog/services | jq '.' | grep jenkins") do
+  its(:exit_status) { should eq 0 }
 end
 
 describe command('curl -s -o /dev/null -w "%{http_code}" http://10.0.2.15/jenkins/') do
